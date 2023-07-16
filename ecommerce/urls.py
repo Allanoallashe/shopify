124e8e4e6245a5a29b3b07c368af7eb0dd49cede
ALLAN LENNY <113612155+Allanoallashe@users.noreply.github.com>
2023-07-16 22:06:46 +0300

    Update urls.py

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns #new
+
 
 
 urlpatterns = [
@@ -24,3 +26,7 @@ urlpatterns = [
     path('',include("ecommerceapp.urls")),
     path('auth/',include("authcart.urls"))
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatters += staticfiles_urlpatterns() #new
+
+
