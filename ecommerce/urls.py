b107add28d527d8f0d2151c8d84d3e07cccf7dd3
ALLAN LENNY <113612155+Allanoallashe@users.noreply.github.com>
2023-07-16 22:09:23 +0300

    Update urls.py

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -27,6 +27,6 @@ urlpatterns = [
     path('auth/',include("authcart.urls"))
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatters += staticfiles_urlpatterns() #new
+urlpatterns += staticfiles_urlpatterns() #new
 
 
